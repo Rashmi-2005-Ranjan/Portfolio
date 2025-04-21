ad4abcd66699dc5621c5db7b2cb2d7af25835b1b
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2025-04-22 00:04:03 +0530

    Company Data Removed

--- my-portfolio/src/constants.js
+++ my-portfolio/src/constants.js
@@ -6,16 +6,15 @@ import javascriptLogo from './assets/tech_logo/javascript.png';
 import reactjsLogo from './assets/tech_logo/reactjs.png';
 import angularLogo from './assets/tech_logo/angular.png';
 import reduxLogo from './assets/tech_logo/redux.png';
-// import nextjsLogo from './assets/tech_logo/nextjs.png';
+import angularMat from './assets/tech_logo/angular-material.png';
 import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
-// import gsapLogo from './assets/tech_logo/gsap.png';
+import nxdev from './assets/tech_logo/nxdev.png';
 import materialuiLogo from './assets/tech_logo/materialui.png';
 import bootstrapLogo from './assets/tech_logo/bootstrap.png';
 import springbootLogo from './assets/tech_logo/springboot.png';
-// import nodejsLogo from './assets/tech_logo/nodejs.png';
-// import expressjsLogo from './assets/tech_logo/express.png';
+import springBootLogo from './assets/tech_logo/spring-boot.png'
+import kotlin from './assets/tech_logo/kotlin.png';
 import mysqlLogo from './assets/tech_logo/mysql.png';
-// import mongodbLogo from './assets/tech_logo/mongodb.png';
 import firebaseLogo from './assets/tech_logo/firebase.png';
 import cLogo from './assets/tech_logo/c.png';
 import cppLogo from './assets/tech_logo/cpp.png';
@@ -26,22 +25,16 @@ import gitLogo from './assets/tech_logo/git.png';
 import githubLogo from './assets/tech_logo/github.png';
 import vscodeLogo from './assets/tech_logo/vscode.png';
 import postmanLogo from './assets/tech_logo/postman.png';
-// import mcLogo from './assets/tech_logo/mc.png';
+import android from './assets/tech_logo/androidLogo.png';
 import figmaLogo from './assets/tech_logo/figma.png';
 import netlifyLogo from './assets/tech_logo/netlify.png';
 import vercelLogo from './assets/tech_logo/vercel.png';
 import postgreLogo from './assets/tech_logo/postgre.png';
-// import csharpLogo from './assets/tech_logo/csharp.png';
-
-// Experience Section Logo's
-// import webverseLogo from './assets/company_logo/webverse_logo.png';
-// import agcLogo from './assets/company_logo/agc_logo.png';
-// import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
 
 // Education Section Logo's
-// import glaLogo from './assets/education_logo/gla_logo.png';
-// import bsaLogo from './assets/education_logo/bsa_logo.png';
-// import vpsLogo from './assets/education_logo/vps_logo.png';
+import cutmLogo from './assets/education_logo/cutm_logo.png';
+import jsrcLogo from './assets/education_logo/jsrc_logo.png';
+import jnpsLogo from "./assets/education_logo/jnps_logo.png";
 
 // Project Section Logo's
 import githubdetLogo from './assets/work_logo/github_det.png';
@@ -66,9 +59,8 @@ export const SkillsInfo = [
       { name: 'React JS', logo: reactjsLogo },
       { name: 'Angular', logo: angularLogo },
       { name: 'Redux', logo: reduxLogo },
-      // { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Angular Material', logo: angularMat },
       { name: 'Tailwind CSS', logo: tailwindcssLogo },
-      // { name: 'GSAP', logo: gsapLogo },
       { name: 'Material UI', logo: materialuiLogo },
       { name: 'Bootstrap', logo: bootstrapLogo },
     ],
@@ -76,11 +68,10 @@ export const SkillsInfo = [
   {
     title: 'Backend',
     skills: [
-      { name: 'Springboot', logo: springbootLogo },
-      // { name: 'Node JS', logo: nodejsLogo },
-      // { name: 'Express JS', logo: expressjsLogo },
+      { name: 'Spring', logo: springbootLogo },
+      { name: 'Spring Boot', logo: springBootLogo },
       { name: 'MySQL', logo: mysqlLogo },
-      // { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Android Development', logo: android },
       { name: 'Firebase', logo: firebaseLogo },
       { name: 'PostgreSQL', logo: postgreLogo },
     ],
@@ -91,8 +82,7 @@ export const SkillsInfo = [
       { name: 'C', logo: cLogo },
       { name: 'C++', logo: cppLogo },
       { name: 'Java', logo: javaLogo },
-      // { name: 'Python', logo: pythonLogo },
-      // { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'Kotlin', logo: kotlin },
       { name: 'JavaScript', logo: javascriptLogo },
       { name: 'TypeScript', logo: typescriptLogo },
     ],
@@ -104,7 +94,7 @@ export const SkillsInfo = [
       { name: 'GitHub', logo: githubLogo },
       { name: 'VS Code', logo: vscodeLogo },
       { name: 'Postman', logo: postmanLogo },
-      // { name: 'Compass', logo: mcLogo },
+      { name: 'Nx.dev', logo: nxdev },
       { name: 'Vercel', logo: vercelLogo },
       { name: 'Netlify', logo: netlifyLogo },
       { name: 'Figma', logo: figmaLogo },
@@ -112,98 +102,35 @@ export const SkillsInfo = [
   },
 ];
 
-  export const experiences = [
-    {
-      id: 0,
-      // img: webverseLogo,
-      role: "Fullstack Developer",
-      company: "Webverse Digital",
-      date: "April 2024 - Present",
-      desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
-      skills: [
-        "HTML",
-        "CSS",
-        "JavaScript",
-        "React JS",
-        "TypeScript",
-        "Node JS",
-        "Tailwind CSS",
-        "MongoDb",
-        "Redux",
-        " Next Js",
-      ],
-    },
-    {
-      id: 1,
-      // img: agcLogo,
-      role: "Fullstack Engineer",
-      company: "Agumentik Group of Companies",
-      date: "July 2023 - March 2024",
-      desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
-      skills: [
-        "ReactJS",
-        "Redux",
-        "JavaScript",
-        "Tailwind CSS",
-        "HTML",
-        "CSS",
-        "SQL",
-      ],
-    },
-    {
-      id: 2,
-      // img: newtonschoolLogo,
-      role: "Frontend Intern",
-      company: "Newton School",
-      date: "September 2021 - August 2022",
-      desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
-      skills: [
-        "HTML",
-        "CSS",
-        "Javascript",
-        "Bootstrap",
-        "Figma",
-        "Material UI",
-      ],
-    },
-  ];
+
   
   export const education = [
     {
       id: 0,
-      // img: glaLogo,
-      school: "GLA University, Mathura",
-      date: "Sept 2022 - July 2024",
-      grade: "7.81 CGPA",
-      desc: "I have completed my Master's degree (MCA) in Computer Applications from GLA University, Mathura. During my time at GLA, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at GLA University has been instrumental in shaping my technical abilities and professional growth.",
-      degree: "Master of Computer Applications - MCA",
+      img: cutmLogo,
+      school: "Centurion University, Bhubaneswar",
+      date: "Sept 2022 - May 2026",
+      grade: "9 CGPA",
+      desc: "I have completed my Bachelor's degree (B.Tech) in Computer Science and Engineering from Centurion University, Bhubaneswar. During my time at Centurion University, I built a solid foundation in programming, software development, and core computer science concepts. My coursework included subjects like Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively engaged in technical workshops and events, which helped sharpen my skills and broaden my understanding of real-world applications. My academic journey at Centurion University has played a vital role in shaping my technical proficiency and professional development.",
+      degree: "Bachelor of Technology - B.Tech (CSE)",
     },
     {
       id: 1,
-      // img: bsaLogo,
-      school: "BSA College, Mathura",
-      date: "Sept 2018 - Aug 2021",
-      grade: "73.2%",
-      desc: "I completed my Bachelor's degree in Computer Science (B.Sc.) from BSA College, Mathura. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at BSA College allowed me to work on projects that applied theoretical concepts to real-world problems.",
-      degree: "Bachelor of Science - BSC (Computer Science)",
+      img: jsrcLogo,
+      school: "Jhadeswar +2 Science Residential College, Balasore",
+      date: "May 2020 - May 2022",
+      grade: "86.33%",
+      desc: "I completed my Class 12 education from Jhadeswar Higher Secondary School, located in Balasore, under the CHSE (Council of Higher Secondary Education), Odisha. My academic stream included Physics, Chemistry, Mathematics, and Biology (PCMB). Although Computer Science was not part of the curriculum, my growing interest in technology encouraged me to pursue self-learning in computing fundamentals, which laid the foundation for my future academic and career choices in the tech domain.",
+      degree: "CHSE(XII) - PCMB",
     },
     {
       id: 2,
-      // img: vpsLogo,
-      school: "Vatsalya Public School Govardhan, Mathura",
-      date: "Apr 2017 - March 2018",
-      grade: "78%",
-      desc: "I completed my class 12 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
-      degree: "CBSE(XII) - PCM with Computer Science",
-    },
-    {
-      id: 3,
-      // img: vpsLogo,
-      school: "Vatsalya Public School Govardhan, Mathura",
-      date: "Apr 2015 - March 2016",
-      grade: "87.5%",
-      desc: "I completed my class 10 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Science with Computer.",
-      degree: "CBSE(X), Science with Computer Application",
+      img: jnpsLogo,
+      school: "Jadunathpur Simanta Highschool, Tiring",
+      date: "November 2018 - March 2020",
+      grade: "72.33%",
+      desc: "I completed my Class 10 education from Jadunathpur Simanta High School, located in Tiring, under the BSE (Board of Secondary Education), Odisha. During this phase, I studied core subjects such as Science, Mathematics, and Social Studies, but Computer Science was not part of the curriculum. Despite this, my curiosity in technology began to grow, which later motivated me to explore programming and computer-related subjects in higher studies.",
+      degree: "BSE(X), Science with Mathematics",
     },
   ];
   
